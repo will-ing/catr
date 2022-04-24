093b21be7bb6a3019a44e7fda3b361fa6990dd3c
willk <will-ing@github.com>
2022-04-23 17:28:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# catr
+
+---
+
+## rust cat using CLI
+
+The CLI app that mimics echo, but written in rust
+
+You have the option -n to omit newline, To use in bash enter:
+
+```terminal
+
+
+or with cargo
+
+
+```
+
+To export output to file:
+
+```terminal
+cargo run -- -n hello world > file.txt
+```
+
+---
+
+## Tools Used
+
+- VS Code
+- Rust
+- clap
+- assert_cmd
+- predicates
+
+---
+
+## Recent Updates
+
+- Created 2022-03-20
+
+---
+
+## Getting Started
+
+Clone this repository to your local machine.
+
+```terminal
+git clone https://github.com/will-ing/catr.git
+```
+
+```terminal
+cd YourRepo/YourProject
+cargo build
+cargo run
+```
+
+Unit testing is included in the project
+
+```terminal
+cargo test
+```
+
+---
+
+---
+
+## Change Log
+
+***The change log will list any changes made to the code base.***
+1.0 App created
+
+---
+
+## Authors
+
+Will Koger\
+March 2022
+
+## References
+
+Rust book\
+OReily Command-Line Rust
---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use clap::{Arg, Command};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("will Koger")
+        .about("Rust cat")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input files")
+                .allow_invalid_utf8(true)
+                .multiple_occurrences(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(e) => eprintln!("Failed to open {}: {}", filename, e),
+            Ok(file) => {
+                for line_result in file.lines() {
+                    let line = line_result?;
+                    println!("{}", line)
+                }
+            }
+        }
+    }
+    Ok(())
+}
